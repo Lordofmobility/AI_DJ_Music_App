85349cb4b3eefde1ca2d5f5feaa778acc26e5ded
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-16 18:35:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-bfunjirdkxndfiqesdkfnqaeiuszxk
-
 song_1 = ""
 song_2 = ""
+leftWristX = ""
+leftWristY = ""
+rightWristX = ""
+rightWristY = ""
 
 function preload() {
-    song_1 = loadSound("Harry_Potter_Theme_Song.mp3")
-    song_2 = loadSound("Believer_Imagine_Dragons.mp3")
+    
 }
 
 function setup() {
@@ -14,6 +15,30 @@ function setup() {
 
     video = createCapture(VIDEO)
     video.hide()
+
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on("pose", gotPoses())
+
+    song_1 = loadSound("Harry_Potter_Theme_Song.mp3")
+    song_2 = loadSound("Believer_Imagine_Dragons.mp3")
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        console.log("Left Wrist X is " + leftWristX + ", Left Wrist Y is " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X is " + rightWristX + ", Right Wrist Y is " + rightWristY);
+    }
 }
 
 function draw() {
