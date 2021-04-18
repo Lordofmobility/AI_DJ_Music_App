f0d096ad4505487ca53dd4fa3702d4e4917e229b
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-18 18:33:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ leftWristX = ""
 leftWristY = ""
 rightWristX = ""
 rightWristY = ""
+scoreleftWrist = 0
+status_song_1 = ""
+status_song_2 = ""
 
 function preload() {
-    
+    song_1 = loadSound("Harry_Potter_Theme_Song.mp3")
+    song_2 = loadSound("Believer_Imagine_Dragons.mp3")
 }
 
 function setup() {
@@ -18,9 +22,6 @@ function setup() {
 
     posenet = ml5.poseNet(video, modelLoaded)
     posenet.on("pose", gotPoses())
-
-    song_1 = loadSound("Harry_Potter_Theme_Song.mp3")
-    song_2 = loadSound("Believer_Imagine_Dragons.mp3")
 }
 
 function modelLoaded() {
@@ -30,7 +31,7 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        
+
         leftWristX = results[0].pose.leftWrist.x
         leftWristY = results[0].pose.leftWrist.y
         console.log("Left Wrist X is " + leftWristX + ", Left Wrist Y is " + leftWristY);
@@ -38,9 +39,24 @@ function gotPoses(results) {
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("Right Wrist X is " + rightWristX + ", Right Wrist Y is " + rightWristY);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score
+        console.log(scoreleftWrist);
     }
 }
 
 function draw() {
     image(video, 0, 0, 550, 550)
+    song_1.isPlaying()
+    status_song_1 = song_1.isPlaying()
+    stroke("red")
+    fill("red")
+    if (scoreleftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20)
+        song_2.stop()
+        if (status_song_1 = false) {
+           song_1.play()
+           document.getElementById("song_name").innerHTML = "Harry Potter Theme Song"
+        }
+    }
 }
